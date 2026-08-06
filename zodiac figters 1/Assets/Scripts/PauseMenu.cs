aab6a473e507ff5ced52e177592b2f39c1eb59d5
AmitAceAvr <amit.avrashmi@gmail.com>
2026-08-06 09:26:25 +0300

    pausemenu
    only the script UI is a work in progress

---
+++ zodiac figters 1/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject commandList;
+
+    private bool isPaused = false;
+
+   private  void Start()
+    {
+       pausePanel.SetActive(false);
+        commandList.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (commandList.activeSelf) CloseCommandList(); // making sure the command list isn't immdiatly open when pausing;
+
+            TogglePause();
+
+        }
+
+    }
+
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        isPaused = true;
+        pausePanel.SetActive(true);
+        commandList.SetActive(false);
+        Time.timeScale = 0f;
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pausePanel.SetActive(false );
+        commandList.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void OpenCommandList()
+    {
+        pausePanel.SetActive(false) ;
+        commandList.SetActive(true) ;
+    }
+
+    public void CloseCommandList()
+    {
+        commandList.SetActive(false);
+        pausePanel.SetActive(true) ;
+    }
+
+}
