61c89cebde0f4208fa922c4ec07c70b369e9e32b
Dolev888 <dolev007001@gmail.com>
2026-09-01 10:48:28 +0300

    fin

--- zodiac figters 1/Assets/Scripts/UI/MainMenu.cs
+++ zodiac figters 1/Assets/Scripts/UI/MainMenu.cs
@@ -6,7 +6,12 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private string GamePlayScene = "GamePlayScene";
     [SerializeField] private GameObject commandListPanel;
     [SerializeField] private GameObject mainMenuBcakground;
-    public void Playgame()
+    [SerializeField] private Animator[] _animator;
+    [SerializeField] private int[] _twinkleOrder;
+    [SerializeField] private float _whaitTime;
+    private float timer;
+    private int twinklecount=0;
+   public void Playgame()
     {
         SceneManager.LoadScene(GamePlayScene);
     }
@@ -41,6 +46,17 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= _whaitTime) 
+        {
+            Debug.Log("helo");
+            timer = 0;
+            _animator[_twinkleOrder[twinklecount]].Play("twinkel");
+            twinklecount++;
+            if (twinklecount>11)
+            {
+                twinklecount=0;
+            }
+        }
     }
 }
